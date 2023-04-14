3c0f1ec1e20c3fe6e9a172d00e29cba02997efff
Bobby Prabowo <prabowo.bobby@gmail.com>
2023-04-15 05:14:57 +0900

    first commit

---
+++ plugin/src/main/java/com/bobbyprabowo/metrics/plugin/MetricsPlugin.kt
@@ -0,0 +1,13 @@
+package com.bobbyprabowo.metrics.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MetricsPlugin : Plugin<Project> {
+
+    private val name: String = "metrics"
+
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
+}
