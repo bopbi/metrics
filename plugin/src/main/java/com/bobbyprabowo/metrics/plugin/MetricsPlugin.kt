754e2b6c5f33b4d738543a3e1a758ab420dec991
Bobby Prabowo <prabowo.bobby@gmail.com>
2023-04-15 07:02:48 +0900

    fix import issue

--- plugin/src/main/java/com/bobbyprabowo/metrics/plugin/MetricsPlugin.kt
+++ plugin/src/main/java/com/bobbyprabowo/metrics/plugin/MetricsPlugin.kt
@@ -1,5 +1,6 @@
 package com.bobbyprabowo.metrics.plugin
 
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -11,8 +12,8 @@ class MetricsPlugin : Plugin<Project> {
         val metricExtension = target.extensions.create(name, MetricExtension::class.java)
 
         target.plugins.withId("com.android.application") {
-//            val androidComponents =
-//                target.extensions.getByType(ApplicationAndroidComponentsExtension::class.java)
+            val androidComponents =
+                target.extensions.getByType(ApplicationAndroidComponentsExtension::class.java)
         }
     }
 }
